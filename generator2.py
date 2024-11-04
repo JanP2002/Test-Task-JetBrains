bab9ee2ebb4316b59dd7ac9b01384f3f20a14b47
JanP2002 <268446@student.pwr.edu.pl>
2024-11-04 18:52:14 +0100

    Adds generator2.py

--- generator2.py
+++ generator2.py
@@ -2,32 +2,45 @@
 import random
 import sys
 
-class RandomNumberGenerator:
+class RandomNumberGenerator2:
+    """
+    Pseudo random number generator
+    Args:
+        start: lower bound for random integers
+        end: upper bound for random integers
+    """
 
     def __init__(self, start=0, end=100):
         self.start = start
         self.end = end
-        ##TODO wyjatek start >= end
+        ##!TODO wyjatek start >= end
+
 
     def get_random(self):
+        """
+        Method generating random integer from [start, end]
+        :return: random integer from [start, end]
+        """
         return random.randint(self.start, self.end)
 
 
 
-random_number_generator = RandomNumberGenerator(-1000, 1000)
+random_number_generator = RandomNumberGenerator2(-1000, 1000)
 while True:
     command = sys.stdin.readline().strip()
     if command == "Shutdown":
+        ##cleaning buffer
         sys.stdout.flush()
         break
     if command == "Hi":
         sys.stdout.write("Hi\n")
+        ##cleaning buffer
         sys.stdout.flush()
     if command == "GetRandom":
         r = random_number_generator.get_random()
         sys.stdout.write(f"{r}\n")
+        ##cleaning buffer
         sys.stdout.flush()
-    #TODO: obsluga braku komendy?
 
 
 
